02896126d541bc729e3514f58f04c6e307be6fee
Tiago Angelo <tiagoangelods.dev@gmail.com>
2023-08-21 15:47:43 -0300

    Incremental Input type export

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,7 @@ export { default as Container, ContainerType } from './Container';
 export { default as Divider, DividerType } from './Divider';
 export { default as Modal, ModalType } from './Modal';
 export { default as Input, InputType } from './Input';
+export { default as IncrementalInput, IncrementalInputType } from './IncrementalInput';
 export { default as Switch, SwitchType } from './Switch';
 export {
   default as ScrollableContainer,
