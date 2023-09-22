88e93e1e6eced0e934b9f5ed265cef30f7190ede
Tiago Angelo <tiagoangelods.dev@gmail.com>
2023-09-21 23:41:56 -0300

    number format

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
+/* eslint-disable no-extend-native */
 export * from './components';
+export interface Number {
+  toCurrency(props: any): string;
+}
+
+Number.prototype.toMoney = function (props: any) {
+  const {
+    style = 'currency',
+    currency = 'BRL',
+    signDisplay = 'never',
+    useGrouping = false,
+    ...rest
+  } = props;
+  const toFormat = Intl.NumberFormat('pt-BR', {
+    style,
+    currency,
+    signDisplay,
+    useGrouping,
+    ...rest,
+  });
+
+  return toFormat.format(this as any);
+};
